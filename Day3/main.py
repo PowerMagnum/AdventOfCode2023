c8c25875b4da67822d0276ae453c4c93c3fb000d
PowerMagnum <salvopollicina2002@gmail.com>
2023-12-06 13:14:37 +0100

    Day3 first

--- Day3/main.py
+++ Day3/main.py
@@ -1,36 +1,63 @@
-with open("example1.txt","r") as file:
+with open("input.txt","r") as file:
     data = file.readlines()
 
 engMat = []
 approved = []
 
 def first(data: str):
+    tot = 0
     for line in data:
         engMat.append(list(line.strip()))
     for x in range(len(engMat)):
-        redingNum = False
+        readingNum = False
         approvedNum = False
+        numCache = ""
         for y in range(len(engMat[0])):
             print(engMat[x][y], end="")
             if engMat[x][y].isdigit():
-                redingNum = True
+                readingNum = True
+                if controlSlot(x,y):
+                    approvedNum = True
+                numCache += engMat[x][y]
+            elif readingNum and approvedNum:
+                readingNum = False
+                approvedNum = False
+                approved.append(numCache)
+                numCache = ""
                 #print("'",end="")
-            print("'" if controlSlot(x,y) else "" , end="")
+            else:
+                numCache = ""
+            #controlSlot(x,y)
+            #print("'" if controlSlot(x,y) and engMat[x][y].isdigit() else "-" , end="")
             #print(controlSlot(x, y), end="")
         print()
+    for k in approved:
+        tot += int(k)
+    print(approved)
+    return tot
 
 def controlSlot(x,y):
     esito = False
+    #print(f"[{engMat[x][y]}]  x:{x} y:{y}")
     for i in range(-1, 2):
+        #print(f"x-shift:{i}")
         if x + i < 0 or x + i >= len(engMat):
+            #print("  pass")
             continue
         for j in range(-1, 2):
-            if y + j < 0 or y + j >= len(engMat[0]) or (x == 0 and y == 0):
+            #print(f"   y-shift:{j}",end="")
+            if y + j < 0 or y + j >= len(engMat[0]) or (i == 0 and j == 0):
+                #print("\n     pass")
                 continue
-            if (not engMat[x][y].isdigit()) and engMat[x][y] != ".":
+            if (not engMat[x+i][y+j].isdigit()) and engMat[x+i][y+j] != ".":
                 esito = True
+                #print("  Y:  "+engMat[x+i][y+j])
+            #else:
+                #print("  -:  "+engMat[x+i][y+j])
+    #print(esito)
     return esito
 
 
-first(data)
+print(first(data))
+
 
